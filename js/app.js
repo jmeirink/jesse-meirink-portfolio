602fe5910d58e5af36448b98a951582f430882f7
Jesse Meirink <jesse@meirink.com>
2022-06-09 12:05:09 -0500

    dynamicaly link projects to html using external project-data js file

--- js/app.js
+++ js/app.js
@@ -1,27 +1,50 @@
-//JS Imports
-// import { projectData } from "./project-data.js";
+// Imports
+import { projectData } from "./project-data.js";
 
 
-// JS Functionality Task 1/3 - Fade home page text in onload
+// JS Functionality Task 1/3 - Dynamicaly link projects to html
+const projectsSection = document.getElementById('my-projects')
+
+let projectMarkup = projectData.map(project =>
+  `
+    <div class="row justify-content-center">
+    <div class="col-4 text-center work-content">
+        <img class ="img-responsive rounded" width="75%" src="${project.image}" alt="">
+    </div>
+    <div class="col-4 work-content">
+        <h1>${project.title}</h1>
+        <p>${project.description}</p>
+        <div>
+            <a href="${project.github}" class="btn btn-primary" target="_blank">GitHub</a>
+            <a href="${project.deployment}" class="btn btn-primary" target="_blank">Deployment</a>
+        </div>
+    </div>
+    </div>
+  `
+).join('')
+
+projectsSection.innerHTML = projectMarkup
+
+
+// JS Functionality Task 2/3 - Fade in home page text upon page load
 let opacity = 0;
 let intervalID = 0;
 window.onload = fadeIn;
 
 function fadeIn() {
-    setInterval(show, 200);
+    setInterval(reveal, 100);
 }
 
-function show() {
-    const body = document.getElementById("home-description");
-    opacity = Number(window.getComputedStyle(body)
-                     .getPropertyValue("opacity"));
+function reveal() {
+    const helloText = document.getElementById("home-description");
+    opacity = Number(window.getComputedStyle(helloText).getPropertyValue("opacity"));
     if (opacity < 1) {
-        opacity = opacity + 0.1;
-        body.style.opacity = opacity
+        opacity = opacity + 0.03;
+        helloText.style.opacity = opacity
     } else {
         clearInterval(intervalID);
     }
 }
 
 
-// JS Functionality Task 2/3 - Dynamicaly link projects to html
+// JS Functionality Task 3/3 - ???
--- js/project-data.js
+++ js/project-data.js
@@ -1,19 +1,14 @@
-// const projects = {
-
-// }
-
-
-
-
-
-
-
-
+let projectData = [
+  {
+    deployment: 'URL goes here',
+    title: 'Tic-Tac-Toe',
+    image: '/images/TicTacToe-Square.png',
+    github: 'https://github.com/jmeirink/ttt-weekend',
+    description: 'This is one of the first applications I created using HTML, CSS and Javascript.'
+  }
+]
 
 
-
-
-
-// export {
-//   projectData
-// }
+export {
+  projectData
+}
