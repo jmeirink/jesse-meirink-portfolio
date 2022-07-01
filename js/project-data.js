67e4cdd1fb60bdee026f40615a17fb4bdce2f01f
Jesse Meirink <jesse@meirink.com>
2022-07-01 01:11:57 -0500

    add new project data

--- js/project-data.js
+++ js/project-data.js
@@ -3,9 +3,16 @@ let projectData = [
   {
     deployment: 'https://tic-tac-toe-jmeirink.netlify.app/',
     title: 'Tic-Tac-Toe',
-    image: '/assets/images/TicTacToe-Square.png',
+    image: '/assets/images/tic-tac-toe-screenshot.png',
     github: 'https://github.com/jmeirink/ttt-weekend',
     description: 'This is one of the first applications I created using HTML, CSS and Javascript.'
+  },
+  {
+    deployment: 'https://brew-guides-jmeirink.herokuapp.com/',
+    title: 'Brew Guides',
+    image: '/assets/images/brew-guides-screenshot.png',
+    github: 'https://github.com/jmeirink/brew-guides',
+    description: 'Welcome to Brew Guides! This app was created for coffee enthusiasts to have a place to share all of their brew recipes in one place.'
   }
 ]
 
