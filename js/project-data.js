76221b7e8f0c0bd62a633e7617dfb5ee4c9f6081
Jesse Meirink <jesse@meirink.com>
2022-07-21 15:19:08 -0500

    add foodie project data

--- js/project-data.js
+++ js/project-data.js
@@ -13,6 +13,13 @@ let projectData = [
     image: '/assets/images/brew-guides-screenshot.png',
     github: 'https://github.com/jmeirink/brew-guides',
     description: 'Welcome to Brew Guides! This app was created for coffee enthusiasts to have a place to share all of their brew recipes in one place.'
+  },
+  {
+    deployment: 'https://foodie-foodfrat.netlify.app/',
+    title: 'Foodie',
+    image: '/assets/images/FoodiePrev.png',
+    github: 'https://github.com/jmeirink/foodie-back-end',
+    description: "This is the front end of a decoupled MERN Stack app that includes JWT Authentication. This is a social media app focused on sharing the user's dining experience. Unlike most restaurant review apps, Foodie allows reviewers to post specific dishes or cocktails and connect with other like-minded 'foodies."
   }
 ]
 
