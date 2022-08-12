cd9d94130de6d50d8c5cc10159bc185c007bd1c6
Jesse Meirink <jesse@meirink.com>
2022-08-12 14:46:00 -0500

    update projects data

--- js/project-data.js
+++ js/project-data.js
@@ -3,28 +3,28 @@ let projectData = [
   {
     deployment: 'https://brew-guides-jmeirink.herokuapp.com/',
     title: 'Brew Guides',
-    image: '/assets/images/brew-guides-screenshot.png',
+    image: '/assets/images/project-screenshots/brew-guides.png',
     github: 'https://github.com/jmeirink/brew-guides',
     description: 'Welcome to Brew Guides! This app was created for coffee enthusiasts to have a place to share all of their brew recipes in one place.'
   },
   {
     deployment: 'https://destination-vacation-jmeirink.herokuapp.com/',
     title: 'Destination Vacation',
-    image: '/assets/images/DV-screenshot.png',
+    image: '/assets/images/project-screenshots/destination-vacation.png',
     github: 'https://github.com/jmeirink/destination-vacation',
     description: "Welcome to Destination Vacation! This is a full-stack Django application. The purpose of this app is to provide a place for users to share vacation locations and activities. Best of all this app is fully scalable, so feel free to browse on your phone, computer, or whichever device you choose. Check out the app for yourself to get some inspiration for your next vacation get away!"
   },
   {
     deployment: 'https://foodie-foodfrat.netlify.app/',
     title: 'Foodie',
-    image: '/assets/images/FoodiePrev.png',
+    image: '/assets/images/project-screenshots/foodie.png',
     github: 'https://github.com/jmeirink/foodie-back-end',
     description: "This is a full-stack MERN application that includes JWT Authentication. This is a social media app focused on sharing the user's dining experience. Unlike most restaurant review apps, Foodie allows reviewers to post specific dishes or cocktails and connect with other like-minded 'foodies."
   },
   {
     deployment: 'https://tic-tac-toe-jmeirink.netlify.app/',
     title: 'Tic-Tac-Toe',
-    image: '/assets/images/tic-tac-toe-screenshot.png',
+    image: '/assets/images/project-screenshots/tic-tac-toe.png',
     github: 'https://github.com/jmeirink/ttt-weekend',
     description: 'This is one of the first applications I created using HTML, CSS and Javascript.'
   }
